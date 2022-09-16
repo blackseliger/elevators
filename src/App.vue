e89d8e1639a5947a941255224af3ebd866fd7d09
Gleb Biryukov <biryukov.gleb.98@mail.ru>
2022-09-16 18:17:17 +0300

    фикс багов с миганием для одной колонки

--- src/App.vue
+++ src/App.vue
@@ -34,8 +34,6 @@
 import LiftPage from "@/components/LiftPage.vue";
 import UiButton from "@/components/UiButton.vue";
 
-// подумать над динамическим изображением кнопок
-
 export default {
   name: "App",
   components: {
@@ -46,21 +44,12 @@ export default {
   data() {
     return {
       levels: [5, 4, 3, 2, 1],
-      liftsAmount: 4,
+      liftsAmount: 2,
       lifts: [],
       floors: [],
-      // lastfloor: "",
-      // queue: [],
-      // pause: { rest: false },
       selected: 0,
-      // classMove: null,
-      // showDirect: null,
       indexStart: false,
-      // move: false,
-      // blink: false,
       actualLift: null,
-      intervalRest: false,
-      // cabinCoords:
     };
   },
 
@@ -76,15 +65,14 @@ export default {
         queue: [],
         pause: { rest: false },
         showDirect: null,
-        move: false,
+        move: true,
         blink: false,
         start: false,
         id: `${i}/lift`,
-        // watchLift: false,
       });
     }
   },
-  // `${100 - Math.floor((1 * 100) / this.levels.length)}px`
+
   computed: {
     widthLifts() {
       return `${this.liftsAmount * 150}px`;
@@ -113,88 +101,71 @@ export default {
   methods: {
     pause() {
       let _this = this.actualLift;
-      _this.showDirect = false;
 
-      if (_this.pause.rest) return null;
-      if (_this.finish === true && _this.move) {
-        // не могу понять в чем баг если выбрать больше 2 этажей, то последний будет бликать 6 секунд. this.queue.shift(); первый раз срабатывает корректно
-        // но сам this.queue для setTimeout не обновляется
-        _this.finish = false;
-        _this.move = false;
-        _this.pause.rest = true;
-        _this.blink = true;
-        setTimeout(() => {
-          if (_this.floors.length === 1) {
-            _this.lastfloor = _this.floors[0];
-            // this.workLocalStorage("lastfloor");
-          }
-          _this.showDirect = false;
-          _this.floors.shift();
-          _this.pause.rest = false;
-          _this.blink = false;
-          _this.queue.shift();
-          this.pushLift();
-          // this.workLocalStorage("floors");
-          // this.workLocalStorage("queue");
-        }, 3000);
-      }
+      setTimeout(() => {
+        // console.log("pause");
+        if (_this.pause.rest) return null;
+        if (_this.finish === true && !_this.move) {
+          _this.pause.rest = true;
+          _this.blink = true;
+          setTimeout(() => {
+            if (_this.floors.length === 1) {
+              _this.lastfloor = _this.floors[0];
+              // this.workLocalStorage("lastfloor");
+            }
+            _this.finish = false;
+            _this.floors.shift();
+            _this.pause.rest = false;
+            _this.blink = false;
+            _this.queue.shift();
+            _this.move = true;
+            // console.log("до повтора", _this.queue.length);
+            if (_this.queue.length) {
+              // console.log("повтор");
+              this.pushLift();
+            }
+            // this.workLocalStorage("floors");
+            // this.workLocalStorage("queue");
+          }, 3000);
+        }
+      }, 0);
     },
 
     pushLift() {
       let _this = this.actualLift;
-      _this.classMove = {
-        top: `${_this.queue[0]?.coords}%`,
-        transition: `top ${_this.queue[0]?.speed}s ease-in-out`,
-      };
+      setTimeout(() => {
+        _this.classMove = {
+          top: `${_this.queue[0]?.coords}%`,
+          transition: `top ${_this.queue[0]?.speed}s ease-in-out`,
+        };
+      }, 0);
 
-      if (!this.move && _this.queue[0]) {
-        setTimeout(() => {
-          _this.finish = true;
-          this.intervalRest = false;
-          this.pause();
-          this.floors.shift();
-        }, _this.queue[0].speed * 1000);
-      }
+      setTimeout(() => {
+        if (_this.queue.length && !_this.pause.rest) {
+          // console.log("работает второй таймаут в pushlift с проверкой 1");
+          _this.showDirect = _this.queue[0]?.direction;
+        }
+        if (_this.move && _this.queue[0]) {
+          console.log("работает второй таймаут в pushlift с проверкой 2");
 
-      _this.move = true;
-      _this.showDirect = _this.queue[0]?.direction;
-
-      // if (_this.pause.rest) return null;
-      // if (_this.finish === true && _this.move) {
-      // if (this.intervalRest) return null;
-      //   this.intervalRest = true;
-      //   // не могу понять в чем баг если выбрать больше 2 этажей, то последний будет бликать 6 секунд. this.queue.shift(); первый раз срабатывает корректно
-      //   // но сам this.queue для setTimeout не обновляется
-      //   _this.finish = false;
-      //   _this.move = false;
-      //   _this.pause.rest = true;
-      //   _this.blink = true;
-      //   console.log('heh')
-      //   setTimeout(() => {
-      //     if (_this.floors.length === 1) {
-      //       _this.lastfloor = _this.floors[0];
-      //       // this.workLocalStorage("lastfloor");
-      //     }
-      //     _this.floors.shift();
-      //     _this.pause.rest = false;
-      //     _this.blink = false;
-      //     _this.queue.shift();
-      //     // this.pushLift();
-      //     // this.intervalRest = false;
-      //     // this.workLocalStorage("floors");
-      //     // this.workLocalStorage("queue");
-      //   }, 3000);
-
-      // }
+          _this.move = false;
+          setTimeout(() => {
+            _this.finish = true;
+            this.floors.shift();
+            _this.showDirect = null;
+            this.pause();
+          }, _this.queue[0].speed * 1000);
+        }
+      });
+      // тут что то не так
+      // setTimeout(() => {
+      //   // _this.move = true;
+      //   // _this.showDirect = _this.queue[0]?.direction;
+      // });
     },
 
     startWork() {
       this.pushLift();
-      // if (this.indexStart === false) {
-      //   this.indexStart = setInterval(() => {
-      //     this.pushLift.call(this);
-      //   }, 1000);
-      // }
     },
 
     workLocalStorage(name) {
@@ -205,79 +176,78 @@ export default {
   watch: {
     selected() {
       const freeLifts = this.lifts.filter(
-        (el) => el.move === false && el.pause.rest === false
+        (el) => el.move === true && el.pause.rest === false
       );
-      console.log(freeLifts);
-      if (freeLifts.length >= 2) {
-        const lastFloors = freeLifts.map(
-          (el) => {
-            let value = el.floors[el.floors.length - 1];
-            return value;
-            } 
-        );
+      console.log(freeLifts.length);
+      if (freeLifts.length > 0) {
+        const lastFloors = freeLifts.map((el) => {
+          let value = el.floors[el.floors.length - 1];
+          return value;
+        });
         const closest = lastFloors.sort(
           (a, b) => Math.abs(this.selected - a) - Math.abs(this.selected - b)
         )[0];
         const actualLiftIndex = this.lifts.findIndex(
           (el) => el.floors[el.floors.length - 1] === closest
         );
-        console.log(actualLiftIndex);
+        // console.log(actualLiftIndex);
         this.actualLift = this.lifts[actualLiftIndex];
         this.actualLift.floors.push(this.selected);
       } else {
         // если не подходит условию просто берем всегда первый лифт
         this.actualLift = this.lifts[0];
-        this.actualLift.floors.push(this.selected);
+
+        // this.actualLift.floors.push(this.selected);
+        this.actualLift.floors = [
+          ...this.actualLift.floors,
+          ...[this.selected],
+        ];
+        // чтоб срабатывал watch
       }
 
       this.floors.push(this.selected);
-      // const actualNum = Math.min(...queueAmount);
-      // if (this.actualLift) this.actualLift === null;
-      // this.watchLift = true;
-      // this.watchLift = false;
-      // this.floors.push(this.selected); старое
-      // нужно закидывать активные лифт в список локал стораджа
       // this.workLocalStorage("floors");
     },
 
     // floors: { старое
-    "actualLift.floors": {
-      deep: true,
-      handler() {
-        let _this = this.actualLift;
-        let actualFloor = _this.floors[_this.floors.length - 1];
-        let prevFloor = _this.floors[_this.floors.length - 2];
+    "actualLift.floors"() {
+      // deep: true,
+      // handler() {
+      let _this = this.actualLift;
+      let actualFloor = _this.floors[_this.floors.length - 1];
+      let prevFloor = _this.floors[_this.floors.length - 2];
 
-        if (actualFloor === prevFloor) return null;
+      if (actualFloor === prevFloor) return null;
 
-        if (_this.floors.length === 1 && prevFloor === undefined) {
-          if (actualFloor === this.lastfloor) {
-            _this.floors.shift();
-            // this.workLocalStorage("floors");
-            return null;
-          }
-
-          _this.queue.push({
-            actualFloor: actualFloor,
-            speed: Math.abs(actualFloor - _this.lastfloor),
-            direction: actualFloor > _this.lastfloor ? "up" : "down",
-            coords: 100 - Math.floor((actualFloor * 100) / this.levels.length),
-          });
-          // this.workLocalStorage("queue");
-        } else {
-          _this.queue.push({
-            actualFloor: actualFloor,
-            speed: Math.abs(actualFloor - prevFloor),
-            direction: actualFloor > prevFloor ? "up" : "down",
-            coords: 100 - Math.floor((actualFloor * 100) / this.levels.length),
-          });
-          // this.workLocalStorage("queue");
+      if (_this.floors.length === 1 && prevFloor === undefined) {
+        if (actualFloor === _this.lastfloor) {
+          _this.floors.shift();
+          // this.workLocalStorage("floors");
+          return null;
         }
-        // this.watchLift = false;
-        this.startWork();
-      },
+
+        _this.queue.push({
+          actualFloor: actualFloor,
+          speed: Math.abs(actualFloor - _this.lastfloor),
+          direction: actualFloor > _this.lastfloor ? "up" : "down",
+          coords: 100 - Math.floor((actualFloor * 100) / this.levels.length),
+        });
+        // this.workLocalStorage("queue");
+      } else {
+        _this.queue.push({
+          actualFloor: actualFloor,
+          speed: Math.abs(actualFloor - prevFloor),
+          direction: actualFloor > prevFloor ? "up" : "down",
+          coords: 100 - Math.floor((actualFloor * 100) / this.levels.length),
+        });
+        // this.workLocalStorage("queue");
+      }
+      // this.watchLift = false;
+      // console.log("queue увеличился");
+      this.startWork();
     },
   },
+  // },
 };
 </script>
 
